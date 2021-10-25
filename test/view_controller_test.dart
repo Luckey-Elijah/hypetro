cf856f2b4f34b026aa724b25c7510c833af19734
Elijah Luckey <luckeyelijah112@gmail.com>
2021-10-25 11:36:02 -0400

    completing testa

--- test/view_controller_test.dart
+++ test/view_controller_test.dart
@@ -1,1 +1,42 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hypetro/hypetro.dart';
+
+import 'mocks.dart';
+
+class MyViewController
+    extends ViewController<MyBloc, int, MockView, MockViewModel> {
+  const MyViewController() : super(key: const Key('MyViewController'));
+
+  @override
+  MockView buildView(
+    BuildContext context,
+    MyBloc bloc,
+    MockViewModel viewModel,
+  ) {
+    return MockView(MockActions(bloc), viewModel);
+  }
+
+  @override
+  MockViewModel stateToViewModel(int state) {
+    return MockViewModel();
+  }
+}
+
+void main() {
+  group('ViewController', () {
+    testWidgets('builds view', (tester) async {
+      final widget = BlocProvider<MyBloc>(
+        create: (context) => MyBloc(),
+        child: const MyViewController(),
+      );
+
+      await tester.pumpWidget(widget);
+      await tester.pump();
+
+      expect(find.byKey(const Key('MyViewController')), findsOneWidget);
+      expect(find.byKey(const Key('MockView')), findsOneWidget);
+    });
+  });
+}
--- test/view_test.dart
+++ test/view_test.dart
@@ -1,1 +1,37 @@
-void main() {}
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hypetro/hypetro.dart';
+
+import 'mocks.dart';
+
+class MyView extends View {
+  const MyView(ViewActions<Bloc> actions, ViewModel viewModel)
+      : super(actions, viewModel, key: const Key('MyView'));
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
+
+void main() {
+  group('View', () {
+    late ViewActions<Bloc> actions;
+    late ViewModel viewModel;
+
+    setUp(() {
+      actions = MockActions(MockMyBloc());
+      viewModel = MockViewModel();
+    });
+
+    testWidgets('builds MyView', (tester) async {
+      final widget = MyView(actions, viewModel);
+
+      await tester.pumpWidget(widget);
+
+      expect(find.byKey(const Key('MyView')), findsOneWidget);
+    });
+  });
+}
